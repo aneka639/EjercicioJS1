820f4bfc4b641dfc14559811f3b323e0fed9a537
Aneka639 <nic.guini@gmail.com>
2020-10-11 15:20:03 -0300

    avance

---
+++ JS3.js
@@ -0,0 +1,16 @@
+var array = ['Pedro', 'Marcelo', 'Tomas', 'Ana', 'Giuiana', 'asdfasfds', 'asdsadf'];
+let cantidad= length
+
+for ()
+
+
+(var i=0;i<7;i++) 
+        (i)
+        (nombreArray)[i]
+    )
+
+function nombreArray (nombre) {
+    
+    alert = ("Este es el nombre " + nombre);
+
+}
