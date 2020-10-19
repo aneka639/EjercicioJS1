7914d71aeb2afe03d25704bd1590601793d5e1fe
Aneka639 <nic.guini@gmail.com>
2020-10-19 20:35:56 -0300

    avance

--- ejercicionodo.js
+++ ejercicionodo.js
@@ -6,4 +6,12 @@
 function funcionJavascript() {
     let variableH1 = document.createElement("h1");
     
+    variableH1.innerHTML = "<span>hola dentro del span</span>";
+
+
+    let div2 = document.getElementById("idDiv2");
+
+    div2.appendChild(variableH1); //con esto le agrega el hijo h1
+
 }
+
