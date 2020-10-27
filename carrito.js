300181876c9c3104cfd6b953a20d4523623a6c44
Aneka639 <nic.guini@gmail.com>
2020-10-26 21:03:32 -0300

    ejercicio carrito

---
+++ carrito.js
@@ -0,0 +1,9 @@
+class comida {
+    constructor(articulo, precio, gustos){
+        this.articulo = articulo;
+        this.precio = precio;
+        this.gustos = gustos;
+    }
+}
+
+var arrayCarrito = [];
